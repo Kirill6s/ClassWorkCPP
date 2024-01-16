addf64173489cb8e9d07b8a5a3451c6930704fb5
Kirill6s <156140123+Kirill6s@users.noreply.github.com>
2024-01-16 13:47:59 +0300

    Add files via upload

--- 15.01.2024/DefaultLess.cpp
+++ 15.01.2024/DefaultLess.cpp
@@ -1,23 +1,92 @@
 #include <iostream>
 using namespace std;
+
+//class Student {
+//private:
+//	string name;
+//	int age;
+//public:
+//	Student(string name, int age) {
+//		this->age = age;
+//		this->name = name;
+//	}
+//	void show() {
+//		cout << "name: " << name << " age: " << age << endl;
+//	};
+//};
+
+//class Student {
+//private:
+//	int numerator;
+//	int denominator;
+//
+//
+//public:
+//	Drob (int numerator, int denominator) {
+//		this->numerator = numerator;
+//		this->denominator = denominator;
+//	}
+//	void show() {
+//		cout << numerator << "/" << denominator <<endl;
+//	}
+//
+//};
+
 class Student {
 private:
+	int number;
 	string name;
-	int age;
+	double avg;
 public:
-	Student(string name, int age) {
-		this->age = age;
+	Student(int number, string name, double avg) {
+		this->number = number;
 		this->name = name;
+		this->avg = avg;
 	}
 	void show() {
-		cout << "name: " << name << " age: " << age << endl;
-	}
-
+		cout << "number:" << number << " name:" << name << "avg:" << avg;
+	};
+	string getStatus() {
+		if (avg == 5) 	return  " Otl";
+		
+		if (avg == 4) 	return " Hor";
+		
+		if (avg == 3) 	return " fri";
+		
+		if (avg == 2)	return " db";
+		else return " S";
+	};
 };
 
+class Sa {
+private:
+	int num;
+	int denum;
+	double total ;
+public:
+	Sa(int num, int denum) {
+		
+			total = num / denum;
+
+		this->num = num % denum;
+		this->denum = denum;
+	}
+	void show() {
+		if (total == 0)
+			cout << total << " " << num << "/" << denum;
+	}
+	/*void res() {
+		this->total = num / denum;
+		int remains = num % denum;
+		if (total > 0) cout << total << " " << remains << "/" << denum;
+	}*/
+};
 
 int main() {
-	Student st1("Pol", 23);
+	
+	Sa st1(4, 7);
 	st1.show();
+
+	return 0;
 	
 }
