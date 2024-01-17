82865739d21cff3d461e6cab3a6ab93c81dfd337
Kirill6s <156140123+Kirill6s@users.noreply.github.com>
2024-01-17 21:11:15 +0300

    Add files via upload

---
+++ 17.01.2024/Source.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//
+//class Car {
+//private:
+//	string color;
+//	string model;
+//	float price;
+//public:
+//	Car() {
+//		cout << "car created" << endl;
+//	}
+//	Car(string color, string model) {
+//		
+//		this->color = color;
+//		this->model = model;
+//
+//	}
+//	Car(string color, string model, float price) :
+//		Car(color, model) {
+//		this->price = price;
+//	}
+//	void setPrice(float price) {
+//		this->price = price;
+//	}
+//	void setColor(string color) {
+//		this->color = color;
+//	}
+//	void setModel(string color) {
+//		this->model = model;
+//	}
+//	string getColor() {
+//		return color;
+//	}
+//	float getPrice() {
+//		return price;
+//	}
+//	string getModel() {
+//		return model;
+//	}
+//	
+//	string toString() {
+//		return "color: " + color + " model: " + model + " price: " + to_string(price);
+//	}
+//};
+//
+//Car getMinPrice(Car& car1, Car car2) {
+//	if (car1.getPrice() < car2.getPrice()) {
+//		return car1;
+//	}
+//	else {
+//		return car2;
+//	}
+//}
+//
+//void changeColor(Car& car) {
+//	car.setColor("gold");
+//}
+//
+//Car retCarMin() {
+//
+//}
+//
+//
+// 
+////Car DSF(Car car3,Car car1) {
+////	car3 = car1;
+////	car3.getPrice() = car1.getPrice();
+////	car3.getPrice() = car3.getPrice() + (car3.getPrice() * 0.01);
+////	return car3;
+////}
+////массив из машин и цвет и возращает массив машин которые возращает машины данного цвета
+//
+//Car getAllCarsWitchColor(Car* cars, int size,string color ) {
+//	int count = 0;
+//	for (int i = 0; i < size; i++) {
+//		if (cars[i].getColor() == color) { count++; }
+//	}
+//	Car* buf = new Car[count];
+//	for (int i = 0; i < size; i++) {
+//		if(buf[k++]
+//	}
+
+
+
+
+class Student {
+	string name
+	float mark;
+public:
+	S
+}
+
+
+int main() {
+	Car* cars = new Car[4]{ Car("black", "lada", 200),
+					Car("black2", "lada2", 300),
+					Car("black3", "lada3", 4000)
+	}
+
+	return 0;
+}
